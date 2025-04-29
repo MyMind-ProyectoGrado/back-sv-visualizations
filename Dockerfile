a99abc27658f4a5b2a8275d6b20a6273d7959450
RhaxCity <juanjogomezarenas1@gmail.com>
2025-04-29 18:14:23 -0500

    fix structure

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.10-slim
 
 # Configurar el directorio de trabajo dentro del contenedor
 WORKDIR /app
+ENV TZ=America/Bogota 
 
 # Copiar los archivos de requerimientos e instalarlos
 COPY requirements.txt .
@@ -12,7 +13,7 @@ RUN python -m pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # Exponer el puerto en el que correrá FastAPI
-EXPOSE 8000
+EXPOSE 8002
 
 # Comando para ejecutar la aplicación con Uvicorn
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8002"]
---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request, HTTPException, Depends
+from app.routes import transcription
+from app.routes import user  # Asegúrate de que sea minúscula "user" en lugar de "User"
+import os
+from dotenv import load_dotenv
+import socket
+
+# Middleware para verificar la fuente de la solicitud
+async def verify_request_from_apisix(request: Request):
+    entorno = os.getenv("ENVIRONMENT")
+
+    if entorno == "production":
+        expected_url = os.getenv("APISIX_PROD")  # (Tendrás que definirlo para producción también)
+        client_ip = request.client.host
+        print(f"Client IP (Production): {client_ip}, Expected URL: {expected_url}")
+
+        if not client_ip.startswith("http"):
+            if client_ip != expected_url:
+                raise HTTPException(status_code=403, detail="Forbidden: Not allowed source")
+    else:
+        # En local, resolver IP del contenedor 'apisix'
+        expected_container_name = "apisix"
+        expected_ip = socket.gethostbyname(expected_container_name)
+        client_ip = request.client.host
+
+        if client_ip != expected_ip:
+            raise HTTPException(status_code=403, detail="Forbidden: Not allowed source")
+
+app = FastAPI(title="MyMind - Visualization Service", dependencies=[Depends(verify_request_from_apisix)])
+
+# Incluye las rutas
+app.include_router(transcription.router, tags=["Transcriptions"])
+
